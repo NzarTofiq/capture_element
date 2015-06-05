ab84e4a701417a9dfcb0c8e5807c3b36de2031df
Nzar Tofiq <nzar.tofiq@bbc.co.uk>
2015-06-05 16:22:21 +0100

    working but taking a shot of the wrong place in the window

--- background.js
+++ background.js
@@ -6,45 +6,33 @@ chrome.browserAction.onClicked.addListener(function(tab){
 
 function onMessage(request, sender) {
     tabId = sender.tab.id;
-    if(request.needsScrolling  === true){
-        console.log(request);
-    }else{
-        capture(tabId, request);
-    }
+    capture(tabId, request);
 }
 
 chrome.runtime.onMessage.addListener(onMessage);
 
 function capture(tabId, dimensions) {
     chrome.tabs.get(tabId, function(tab) {
+        console.log(dimensions);
         chrome.tabs.captureVisibleTab(tab.windowId, { format: "png" }, function(dataUrl) {
             var canvas = document.createElement("canvas");
+            pixelDensity = window.devicePixelRatio;
             document.body.appendChild(canvas);
             var image = new Image();
             image.onload = function() {
-                pixelDensity = dimensions.pixelDensity;
                 canvas.width = dimensions.width * pixelDensity;
                 canvas.height = dimensions.height * pixelDensity;
                 var context = canvas.getContext("2d");
-                context.drawImage(image,
-                    dimensions.left * pixelDensity, dimensions.top * pixelDensity,
+                context.drawImage(image, 
+                    dimensions.left * pixelDensity, dimensions.top * pixelDensity, 
                     dimensions.width * pixelDensity, dimensions.height * pixelDensity,
                     0, 0,
-                    dimensions.width * pixelDensity, dimensions.height * pixelDensity
-                );
+                    dimensions.width * pixelDensity, dimensions.height * pixelDensity);
                 var croppedDataUrl = canvas.toDataURL("image/png");
-                chrome.tabs.sendMessage(tab.id, {msg: "imageUrl", url: canvas.toDataURL("image/png")});
+                chrome.tabs.create({url : croppedDataUrl});
+                //chrome.tabs.sendMessage(tab.id, {msg: "imageUrl", url: canvas.toDataURL("image/png")});
             }
             image.src = dataUrl;
         });
     });
-}
-
-function tieCanvases(canvases){
-    canv = document.createElement('canvas');
-    context = canv.getContext('2d');
-    for (var i=0; i<canvases.length; i++){
-        var can3 = document.getElementById('canvas3');
-        context.drawImage(can, 0, canv.height);
-    }
 }
--- contentscript.js
+++ contentscript.js
@@ -1,8 +1,8 @@
 function startCapture(query){
-	if(document.querySelector(query)){
+	try {
 		return document.querySelector(query);
-	}else{
-		return 'error: Element does not exist in the context of this document'
+	}catch(error){
+		throw error;
 	}
 }
 
@@ -10,75 +10,50 @@ function send(request) {
     chrome.runtime.sendMessage(request, function(response) {});
 }
 
-function form(){
-	form = document.createElement('form');
-	form.style.background = 'red';
-	form.style.position = 'fixed';
-	form.style.top = '93vh';
-	form.style.right = '0';
-	form.style.padding = '2vh';
-	form.style.zIndex = '100000';
-	form.style.display = 'inline-block'
-	form.innerHTML = '<input id="capture_id_text" type="textbox"></input> <input id="capture_id_button" type="submit"></input>'
-	document.body.appendChild(form);
-	return form;
-}
-
-function setDimentions(element, x, y, originalX, originalY){
-	var top = y;
-	var left = x;
-	var height = element.offsetHeight;
-	var width = element.offsetWidth;
-	var pixelDensity = window.devicePixelRatio;
-	dimensions = {'top': top, 'left': left, 'height': height, 'width': width, 'pixelDensity': pixelDensity};
-	window.scrollTo(originalX,originalY);
+function setDimensions(element){
+	pixelDensity = 1/window.devicePixelRatio;
+	var top = element.offsetTop * pixelDensity;
+	var left = element.offsetLeft * pixelDensity;
+	var height = element.clientHeight;
+	var width = element.clientWidth;
+	dimensions = {'left': left, 'top': top, 'height': height, 'width': width};
 	return dimensions;
 }
 
-function dimentions(element){
-	originalX = window.scrollX;
-	originalY = window.scrollY;
-	if(element.offsetHeight > window.innerHeight){
-		y = element.scrollTop;
-		x = element.scrollLeft;
-		send({'needsScrolling' : true})
-		window.scrollTo(0, element.offsetTop);
-		dimensions = setDimentions(element, x, y, originalX, originalY);
-		return dimensions;
-	}else{
-		window.scrollTo(0, element.offsetTop)
-		return setDimentions(element);
-	}
-
-}
-
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+	var form = function (){
+		form = document.createElement('form');
+		form.style.background = 'red';
+		form.style.position = 'fixed';
+		form.style.top = '93vh';
+		form.style.right = '0';
+		form.style.padding = '2vh';
+		form.style.zIndex = '100000';
+		form.style.display = 'inline-block'
+		form.innerHTML = '<input id="capture_id_text" type="textbox"></input> <input id="capture_id_button" type="submit"></input>'
+		document.body.appendChild(form);
+		return form;
+	}();
+
 	if (request.msg === 'start'){
-		if(!!document.querySelector('.guide-card')){
-			element = document.querySelector('.guide-card');
-			dimentions = dimentions(element);
-			send(dimentions);
-		}else{
-			form();
-			document.querySelector('#capture_id_button').addEventListener('click', function(e){
-				e.preventDefault();
-				query = document.querySelector('#capture_id_text').value;
-				document.body.removeChild(form);
-				window.setTimeout(function(){
-					element = startCapture(query);
-					dimentions = dimentions(element);
-					send(dimentions);
-				}, 200);
-			});
-		}
+		document.querySelector('#capture_id_button').addEventListener('click', function(e){
+			e.preventDefault();
+			query = document.querySelector('#capture_id_text').value;
+			document.body.removeChild(form);
+			window.setTimeout(function(){
+				element = startCapture(query);
+				dimentions = setDimensions(element);
+				send(dimentions);
+			}, 200);
+		});
 		
 	} else if (request.msg === 'imageUrl') {
-		var downloadLink = document.createElement('a');
-		downloadLink.innerHTML = 'Click here';
-		downloadLink.download = 'screenshot.png';
-		downloadLink.href = request.url;
-		document.body.appendChild(downloadLink);
-		downloadLink.click();
-		document.body.removeChild(downloadLink);
+		// var downloadLink = document.createElement('a');
+		// downloadLink.innerHTML = 'Click here';
+		// downloadLink.download = 'screenshot.png';
+		// downloadLink.href = request.url;
+		// document.body.appendChild(downloadLink);
+		// downloadLink.click();
+		// document.body.removeChild(downloadLink);
 	}
 });
